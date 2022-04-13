27c2b976acfdf355f4888cbb2d270a2fcd68e31b
kinnalover <kinnalover@gmail.com>
2022-04-14 03:03:13 +0600

    first commit2

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from job_functions import start_job,  start_new_job
 from model import create_companies_db, create_table, delete_all_info_companies
 
 #Запускаем чтоб начать новую работу
-#start_new_job()
+start_new_job()
 
 
 #Запускаем, чтоб продолжить работу
-start_job()
-# conn = sqlite3.connect('tasks.db')
-# cur = conn.cursor()
-# cur.execute(f"update tasks SET progress ='Not Started' where website ='http://akm.kgd.gov.kz'")
+#start_job()
+
 
 
 
